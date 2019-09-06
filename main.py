4f5961efbd86640f8911491c028a1329118cd74f
Sasu <sasuolander@yahoo.com>
2019-09-06 15:00:36 +0300

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask,json,jsonify,render_template
+import pandas
+import statistics
+from datetime import datetime
+
+app =  Flask(__name__)
+
+CSVFILE= "./resource/jobentry_export_2019-8-23T9_59vanilla.csv"
+
+def csvCoverter(column):
+    file= pandas.read_csv(CSVFILE,delimiter=";")
+    array= file[column]
+    return array
+
+def median():
+    listOfPageView = csvCoverter("pageviews_all")
+    return statistics.median(listOfPageView)
+
+def average():
+    listOfPageView = csvCoverter("pageviews_all")
+    return(statistics.mean(listOfPageView))
+
+def createArrayOfTime(endDateArray,startDateArray):
+    timeArray = []
+    format="%d.%m.%Y"
+    for (endDate, startdate) in zip(endDateArray, startDateArray):
+        delta=datetime.strptime(endDate, format)-datetime.strptime(startdate, format)
+        timeArray.append(delta.days)
+    return timeArray
+
+def readingTimeToClick():
+    listOfPageView = csvCoverter("pageviews_all")
+    listOfClick = csvCoverter("applyclicks_all")
+    listTimeOfOpen=createArrayOfTime(csvCoverter("date_ends"),csvCoverter("date_posted"))
+    positionList=[]
+    for (pageView,click,timeOfOpen) in zip(listOfPageView,listOfClick,listTimeOfOpen):
+        valuesDictionary ={"pageView":pageView,"click":click,"timeOfOpen":timeOfOpen}
+        positionList.append(valuesDictionary)
+    return positionList
+
+@app.route("/api/stats")
+def statsController():
+    ##print("average",average())
+    ##print("median",median())
+    return jsonify(status="success",stats={"median":median(),"average":average(),"readingTimeToClick":readingTimeToClick()})
+
+@app.route("/")
+def homeController():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ static/js/index.js
@@ -0,0 +1,99 @@
+$(function() {
+  function statsGet() {
+    return axios.get("/api/stats");
+  }
+
+  statsGet().then(function(response) {
+    const data = response.data.stats;
+    const maxValueY = Math.max.apply(
+      Math,
+      data["readingTimeToClick"].map(function(item) {
+        return item.timeOfOpen;
+      })
+    );
+    const minValueY = Math.min.apply(
+      Math,
+      data["readingTimeToClick"].map(function(item) {
+        return item.timeOfOpen;
+      })
+    );
+    $("#result").append(
+      `<p>mediaani: ${data["median"]}</p><p>keskiarvo: ${data["average"]}</p>`
+    );
+
+    const ReadTimeToOpen = document
+      .getElementById("ReadTimeToOpen")
+      .getContext("2d");
+    const ClikTimeToOpen = document
+      .getElementById("ClikTimeToOpen")
+      .getContext("2d");
+    const ReadTimeToOpenChart = new Chart(ReadTimeToOpen, {
+      type: "line",
+      data: {
+        labels: data["readingTimeToClick"]
+          .map(function(item) {
+            return item.pageView;
+          })
+          .sort((a, b) => a - b),
+        datasets: [
+          {
+            label: "Lukukerrat / Päivää auki",
+            data: data["readingTimeToClick"].map(function(item) {
+              return item.timeOfOpen;
+            }),
+            backgroundColor: "rgba(0, 196, 177, 0.2)",
+            borderColor: "rgb(0, 196, 177)",
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: minValueY,
+                max: maxValueY,
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+    const ClikTimeToOpenChart = new Chart(ClikTimeToOpen, {
+      type: "line",
+      data: {
+        labels: data["readingTimeToClick"]
+          .map(function(item) {
+            return item.click;
+          })
+          .sort((a, b) => a - b),
+        datasets: [
+          {
+            label: "Hakuklikkejä / Päivää auki",
+            data: data["readingTimeToClick"].map(function(item) {
+              return item.timeOfOpen;
+            }),
+
+            backgroundColor: "rgba(0, 196, 177, 0.2)",
+            borderColor: "rgb(0, 196, 177)",
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: minValueY,
+                max: maxValueY
+              }
+            }
+          ]
+        }
+      }
+    });
+  });
+});
